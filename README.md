46c86eb6825723174ff5314cd6ca0400d84ee49c
Karim Hussein <karim.hussien13@gmail.com>
2023-10-02 14:09:21 +0300

    added some error handling to power.sh

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 simple shell scripts i use with [dwmblocks](https://github.com/torrinfail/dwmblocks) and [dwm](https://github.com/karimhussein1/dwm)
 - cpu_status.sh
 - - used to show cpu usage and it gets updated every second with dwmblocks
-- kdecon_multishare.sh
+- kdecon_multishare.sh (requires kdeconnect)
 - - shell script for sending multiple files by copying their paths to the clipboard
 - layoutstatus.sh
 - - used to show the current keyboard layout in the status bar
--- power.sh
+++ power.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 # dmenu script used with a keybinding tool like sxhkd to interact with dmenu
 choice=$(echo "Shutdown\nReboot\nExit\nSleep\nHibernate\nSwitch OS" | dmenu -i -p "choose action: ")
-[ $choice = "Shutdown"	] && doas poweroff
-[ $choice = "Reboot"	] && doas reboot
-[ $choice = "Exit"	] && pkill dwm
-[ $choice = "Sleep"	] && doas zzz && betterlockscreen -l
-[ $choice = "Hibernate"	] && doas ZZZ && betterlockscreen -l
-[ $choice = "Switch OS"	] && doas ZZZ -R
+if [ -n "$choice" ]; then
+  [ $choice = "Shutdown"	] && doas poweroff
+  [ $choice = "Reboot"	] && doas reboot
+  [ $choice = "Exit"	] && pkill dwm
+  [ $choice = "Sleep"	] && doas zzz && betterlockscreen -l
+  [ $choice = "Hibernate"	] && doas ZZZ && betterlockscreen -l
+  [ $choice = "Switch OS"	] && doas ZZZ -R
+fi
